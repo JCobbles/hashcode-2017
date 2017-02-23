33d9d80714d7c6be69ed5878d0d028d3705b70aa
Jacob Moss <cob.mossy@gmail.com>
2017-02-23 19:13:46 +0000

    remove dupe description model

--- src/hashcode/HashCode.java
+++ src/hashcode/HashCode.java
@@ -6,6 +6,7 @@
 package hashcode;
 
 import hashcode.Models.Endpoint;
+import hashcode.Models.RequestDescription;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
