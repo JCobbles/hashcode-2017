4552167724689a3ea492b6b2ddaccfe97b561d0d
Aleksei Rozhnov <alekseyrozh@gmail.com>
2017-02-23 18:51:00 +0000

    more constuctors

--- src/hashcode/Models/Endpoint.java
+++ src/hashcode/Models/Endpoint.java
@@ -1,5 +1,6 @@
 package hashcode.Models;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Endpoint
@@ -7,4 +8,15 @@ public class Endpoint
     //cacheServerIndex, latencyToDataCenter
     private Map<Integer, Integer> map;
     private int latencyToDataCenter;
+
+    public Endpoint(int latencyToDataCenter)
+    {
+        map = new HashMap<>();
+        this.latencyToDataCenter = latencyToDataCenter;
+    }
+
+    public void addCacheServer(int id, int latency)
+    {
+        map.put(id, latency);
+    }
 }
--- src/hashcode/Models/Video.java
+++ src/hashcode/Models/Video.java
@@ -3,4 +3,9 @@ package hashcode.Models;
 public class Video
 {
     int size;
+
+    public Video(int size)
+    {
+        this.size = size;
+    }
 }
