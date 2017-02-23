f3736e4c42ccbf315678e57ad9525b63179e7eb7
Jacob Moss <cob.mossy@gmail.com>
2017-02-23 18:46:08 +0000

    parser

--- src/hashcode/HashCode.java
+++ src/hashcode/HashCode.java
@@ -20,20 +20,40 @@ public class HashCode {
      */
     public static void main(String[] args) throws FileNotFoundException {
         Scanner keyboard = new Scanner(System.in);
+        int numberOfVideos, numberOfEndpoints, numberOfRequestDescriptions, numberOfCacheServers, cacheServerSize;
 
-        // Get the filename.
         System.out.print("Enter the filename: ");
         String filename = keyboard.nextLine();
 
-        // Open the file.
         File file = new File(filename);
         Scanner inputFile = new Scanner(file);
 
-        int lineNumber = 0;
-        while (inputFile.hasNext()) {
-            lineNumber++;
-            
+        // description line
+        String s = inputFile.next();
+        String[] description = s.split(" ");
+        numberOfVideos = Integer.parseInt(description[0]);
+        numberOfEndpoints = Integer.parseInt(description[1]);
+        numberOfRequestDescriptions = Integer.parseInt(description[2]);
+        numberOfCacheServers = Integer.parseInt(description[3]);
+        cacheServerSize = Integer.parseInt(description[4]);
+        int[] videos = new int[numberOfVideos];
+        
+        // videos line
+        String[] videosDescription = inputFile.next().split(" ");
+        
+        for (int i = 0; i < numberOfVideos; i++) {
+            videos[0] = Integer.parseInt(videosDescription[i]);
         }
+        
+        for (int i = 0; i < numberOfEndpoints; i++) {
+            String[] endpointDescription = inputFile.next().split(" ");
+            int latency = Integer.parseInt(endpointDescription[0]);
+            int cachesAttached = Integer.parseInt(endpointDescription[1]);
+            for (int j = 0; j < endpointDescription.length; j++) {
+                String[] cacheDescription = inputFile
+            }
+        }
+        
     }
 
 }
